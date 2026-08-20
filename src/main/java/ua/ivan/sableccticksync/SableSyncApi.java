c1e52c375ec32c2e6ac5d580114a58159a7aed82
OneOne11 <157164010+323erir6@users.noreply.github.com>
2026-08-20 05:19:44 +0300

    Make Sable Lua API use cached construction state

--- src/main/java/ua/ivan/sableccticksync/SableSyncApi.java
+++ src/main/java/ua/ivan/sableccticksync/SableSyncApi.java
@@ -6,17 +6,25 @@ import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.lua.LuaFunction;
 
 public final class SableSyncApi implements ILuaAPI {
-    private final IComputerSystem computer;
+    private static final String[] API_NAME = new String[]{"sableSync"};
+    private static final String[] NO_NAMES = new String[0];
+
     private final int computerId;
 
     public SableSyncApi(IComputerSystem computer) {
-        this.computer = computer;
         this.computerId = computer.getID();
+        // Clear stale state if the same numeric computer ID is recreated.
+        HighFrequencyController.removeComputer(this.computerId);
     }
 
     @Override
     public String[] getNames() {
-        return new String[]{"sableSync"};
+        // Do not query Sable/world state here: this is evaluated by CC's computer
+        // thread while the Lua machine is created. The main server thread updates
+        // the cache before Computer.tick() is allowed to start the machine.
+        return HighFrequencyController.isOnConstruction(this.computerId)
+                ? API_NAME
+                : NO_NAMES;
     }
 
     @LuaFunction
@@ -46,7 +54,7 @@ public final class SableSyncApi implements ILuaAPI {
     }
 
     private void requireSableConstruction() throws LuaException {
-        if (!PhysicsComputerTicker.usesPhysicsTicks(this.computer)) {
+        if (!HighFrequencyController.isOnConstruction(this.computerId)) {
             HighFrequencyController.disable(this.computerId);
             throw new LuaException("sableSync is only available while this computer is on an active Sable construction");
         }
