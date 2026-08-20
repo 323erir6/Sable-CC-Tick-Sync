bddd7e5f9af00e3f93f60c7afeeb8abda8fc55cf
OneOne11 <157164010+323erir6@users.noreply.github.com>
2026-08-20 04:56:08 +0300

    Register Sable-only built-in CC API

--- src/main/java/ua/ivan/sableccticksync/SableCcTickSync.java
+++ src/main/java/ua/ivan/sableccticksync/SableCcTickSync.java
@@ -1,5 +1,6 @@
 package ua.ivan.sableccticksync;
 
+import dan200.computercraft.api.ComputerCraftAPI;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
@@ -9,6 +10,12 @@ public final class SableCcTickSync {
     public static final String MOD_ID = "sable_cc_tick_sync";
 
     public SableCcTickSync() {
+        ComputerCraftAPI.registerAPIFactory(computer ->
+            PhysicsComputerTicker.usesPhysicsTicks(computer)
+                ? new SableSyncApi(computer)
+                : null
+        );
+
         NeoForge.EVENT_BUS.addListener(
             EventPriority.HIGHEST,
             PhysicsComputerTicker::onPrePhysicsTick
